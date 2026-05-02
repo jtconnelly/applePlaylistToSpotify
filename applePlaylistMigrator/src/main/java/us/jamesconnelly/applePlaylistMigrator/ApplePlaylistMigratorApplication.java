df20b347ee8b32caba30db68047a24194c8ab2d3
James Connelly - Linux <j.t.connelly@email.msmary.edu>
2026-05-02 11:36:20 -0400

    Adding Spring Project

---
+++ applePlaylistMigrator/src/main/java/us/jamesconnelly/applePlaylistMigrator/ApplePlaylistMigratorApplication.java
@@ -0,0 +1,13 @@
+package us.jamesconnelly.applePlaylistMigrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplePlaylistMigratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplePlaylistMigratorApplication.class, args);
+	}
+
+}
---
+++ applePlaylistMigrator/src/test/java/us/jamesconnelly/applePlaylistMigrator/ApplePlaylistMigratorApplicationTests.java
@@ -0,0 +1,13 @@
+package us.jamesconnelly.applePlaylistMigrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplePlaylistMigratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
