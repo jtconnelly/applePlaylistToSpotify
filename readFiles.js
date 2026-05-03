b2310d5b142f5fc6f19c94e0da18afa8a07c25c1
James Connelly - Linux <j.t.connelly@email.msmary.edu>
2026-05-03 13:19:07 -0400

    Updated existing txt files, added some cleanup for reading in playlist files

--- readFiles.js
+++ readFiles.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 
-function readFiles(dirName, playlistDict)
+function readFiles(dirName, playlistDict, delimiter=",")
 {
     var files = fs.readdirSync(dirName);
     for (var file of files)
@@ -12,17 +12,18 @@ function readFiles(dirName, playlistDict)
                 continue;
             }
             const data = fs.readFileSync(dirName + file, "utf-8");
-            playlistDict[file] = []
+            const playlistName = file.split(".")[0];
+            playlistDict[playlistName] = [];
             var songs = data.split("\n");
             for (var song of songs)
             {
                 // console.log("Data response: " + playlistData.body);
-                var artist = song.split(",").slice(-1);
-                var title = song.split(",").slice(0,-1);
+                var artist = song.split(delimiter).slice(-1);
+                var title = song.split(delimiter).slice(0,-1);
                 var obj = new Object();
                 obj.artist = artist;
                 obj.name = title;
-                playlistDict[file].push(obj);
+                playlistDict[playlistName].push(obj);
             }
         }
         catch(err)
